e95fc84fca82a236026b00ccb0bab458936f9f38
Italo Medici <59889993+ItaloMedici@users.noreply.github.com>
2025-05-25 22:34:22 -0300

    chore: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Providers } from "./providers.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Providers />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<Providers />);
